124cb47d60465da69927ace33ebba8e565a670c6
beth92 <bethtownsend92@gmail.com>
2017-12-05 11:20:19 -0500

    added projects route

--- server.js
+++ server.js
@@ -71,6 +71,13 @@ app.get('/about', (req,res)=>{
   });
 });
 
+// Make a projects page
+app.get('/projects', (req,res)=>{
+  res.render('projects.hbs', {
+    pageTitle: 'My Projects'
+  });
+});
+
 app.get('/bad', (req,res)=>{
   res.send({
     errorMessage: 'Unable to handle request'
